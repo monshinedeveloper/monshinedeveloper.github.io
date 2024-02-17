58e275b1e852a8b88d78a38ea4eaebba0d544940
monshinedeveloper <160322985+monshinedeveloper@users.noreply.github.com>
2024-02-17 16:25:18 +0100

    delete popup.js

--- assets/script/popup.js
+++
@@ -1,9 +0,0 @@
-function openPopup() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "block";
-}
-
-function closePopup() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "none";
-}
