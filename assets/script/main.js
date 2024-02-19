fd65c7dc4ca343b8f62561361e93911f004b1ebf
monshinedeveloper <160322985+monshinedeveloper@users.noreply.github.com>
2024-02-19 13:20:16 +0100

    update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,9 @@
-
+function copyToClipboard(username) {
+  const el = document.createElement('textarea');
+  el.value = username;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  alert('Username "' + username + '" wurde in die Zwischenablage kopiert.');
+}
